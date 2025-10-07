57d7182035c53421463210c2ada58a2c8329ec06
Deepesh <deepeshkr25899@gmail.com>
2025-10-07 13:42:34 +0530

    meth method is added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,30 +1,31 @@
 package com.driver;
 
- class A 
- {
-   public string meth()
-    {
-       return " invoking method from class A" ;   	
-    }	 
- }
-class B extends  A
-{  
-	@override
-	public string meth()
-	{
-		return "method is overridden in class B";
-	}
-       
-}
 public class Main 
 {
+	 class A 
+	 {
+	   public string meth()
+	    {
+	       return " invoking method from class A" ;   	
+	    }	 
+	 }
+	class B extends  A
+	{  
+		//@override
+		public string meth()
+		{
+			return "method is overridden in class B";
+		}
+	       
+	}
+	
 	public static void main(String [] args)
 	{
 		B object=new B();
 	       object.meth();
 	       
 	       B object1=new B();
-	       object2.meth();
+	       object1.meth();
 	}
 	
 	
